3f56d6a1c73921bb0fdb8d03de852139be0e8b37
Boubacar S. BARRY <bouba@hackages.io>
2016-11-09 16:59:24 +0100

    Import CartItemInfoComponent

--- src/app/cart-list/cart-list.module.ts
+++ src/app/cart-list/cart-list.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CartListComponent } from './cart-list.component';
 import { CartItemModule } from './cart-item';
+import { CartItemInfoComponent } from './cart-item-info';
 @NgModule({
   imports: [CartItemModule],
   exports:[CartListComponent],
-  declarations: [CartListComponent],
+  declarations: [CartListComponent, CartItemInfoComponent],
   providers:[]
 })
 export class CartListModule{
