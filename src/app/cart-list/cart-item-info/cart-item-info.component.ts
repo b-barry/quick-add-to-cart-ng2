c11c364401fbc06c8d92e8299409e9e117e83424
Boubacar S. BARRY <bouba@hackages.io>
2016-11-09 16:56:23 +0100

    Add boilerplate files

---
+++ src/app/cart-list/cart-item-info/cart-item-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cart-item-info',
+  templateUrl: './cart-item-info.component.html',
+  styleUrls:[ './cart-item-info.component.css']
+})
+export class CartItemInfoComponent {
+
+}
