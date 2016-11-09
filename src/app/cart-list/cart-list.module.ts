9d19762ba074827441c3807674de38ae1ea3554d
Boubacar S. BARRY <bouba@hackages.io>
2016-11-09 19:44:59 +0100

    Refractor cart list component + add common module

--- src/app/cart-list/cart-list.module.ts
+++ src/app/cart-list/cart-list.module.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CartListComponent } from './cart-list.component';
 import { CartItemModule } from './cart-item';
 import { CartItemInfoComponent } from './cart-item-info';
 @NgModule({
-  imports: [CartItemModule],
+  imports: [CartItemModule, CommonModule],
   exports:[CartListComponent],
   declarations: [CartListComponent, CartItemInfoComponent],
-  providers:[]
+  providers: [],
 })
 export class CartListModule{
 
