27d3bce8d657f24ff0a9ce5cde4b89f8789434bc
Boubacar S. BARRY <bouba@hackages.io>
2016-11-09 19:41:52 +0100

    Add input product property

--- src/app/cart-list/cart-list.component.ts
+++ src/app/cart-list/cart-list.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cart-list',
   templateUrl: './cart-list.component.html',
-  styleUrls:[ './cart-list.component.css']
+  styleUrls: ['./cart-list.component.css'],
+  
 })
 export class CartListComponent {
+  @Input() products = [];
 
 }
