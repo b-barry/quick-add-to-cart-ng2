6c6fefa3417892de2355d143340c400d88d53bd5
Boubacar S. BARRY <bouba@hackages.io>
2016-11-20 17:15:58 +0100

    Move product api call to cart list component

--- src/app/cart-list/cart-list.component.ts
+++ src/app/cart-list/cart-list.component.ts
@@ -1,14 +1,38 @@
-import { Component, Input } from '@angular/core';
-import { Product, Item} from '../service';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductApiService } from '../service';
+import { Product, Item } from '../service';
 @Component({
   selector: 'cart-list',
   templateUrl: './cart-list.component.html',
   styleUrls: ['./cart-list.component.css'],
+  providers: [ProductApiService]
 
 })
 export class CartListComponent {
   @Input() products: Product[] = [];
 
+  constructor(private productApi: ProductApiService) {
+  }
+  ngOnInit() {
+    // ES6 version
+    this.productApi
+      .getProducts()
+      .then((products) => {
+        this.products = products;
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      })
+    // Traditional version
+    // this.productApi
+    //   .getProducts()
+    //   .then(function (products) {
+    //     this.products = products;
+    //   }.bind(this))
+    //   .catch((err) => {
+    //     console.log('Error', err);
+    //   })
+  }
   getPriceOfSelectedItemFromProduct(product: Product): string {
     const colorSelected = product
       .items
